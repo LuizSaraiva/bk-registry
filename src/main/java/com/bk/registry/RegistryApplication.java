477247f8332b660865e23ec674551d25974e102e
Luiz <l.henrique.saraiva@gmail.com>
2024-09-22 10:59:59 +0100

    feat: created schedule to outbox

--- src/main/java/com/bk/registry/RegistryApplication.java
+++ src/main/java/com/bk/registry/RegistryApplication.java
@@ -2,11 +2,13 @@ package com.bk.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class RegistryApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
---
+++ src/main/java/com/bk/registry/schedulers/SendMessageToBroker.java
@@ -0,0 +1,60 @@
+package com.bk.registry.schedulers;
+
+import com.bk.registry.domain.entity.OutboxRegistry;
+import com.bk.registry.domain.enums.TypeOutbox;
+import com.bk.registry.domain.publishers.EventPublisher;
+import com.bk.registry.domain.services.OutboxRegistryService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Log4j2
+public class SendMessageToBroker {
+
+    @Value(value = "${rabbitmq.broker.routing.account}")
+    private String routingAccount;
+
+    @Value(value = "${rabbitmq.broker.exchange}")
+    private String exchangeRegistry;
+
+    @Autowired
+    private EventPublisher eventPublisher;
+
+    @Autowired
+    private OutboxRegistryService outboxRegistryService;
+
+    @Scheduled(initialDelay = 10, fixedDelay = 20, timeUnit = TimeUnit.SECONDS)
+    public void scheduleOutbox(){
+
+        List<OutboxRegistry> outboxNotSent = outboxRegistryService.getOutboxNotSent();
+        log.info(String.format("%s messages found to be sent to the broker",outboxNotSent.size()));
+
+        try {
+            outboxNotSent.forEach(outbox ->{setRoutingKeyAndSendMessage(outbox);});
+        }catch (RuntimeException ex){
+            log.error(ex);
+        }
+
+    }
+
+    private void setRoutingKeyAndSendMessage(OutboxRegistry outboxRegistry){
+        TypeOutbox outboxRegistryType = outboxRegistry.getType();
+
+        switch (outboxRegistryType){
+            case ACCOUNT -> {
+                eventPublisher.publisherEvent(exchangeRegistry, routingAccount, outboxRegistry.getMessage());
+                outboxRegistry.setSent(true);
+                outboxRegistry.setUpdate_date(OffsetDateTime.now());
+                outboxRegistryService.saveOutbox(outboxRegistry);
+            }
+            default -> log.error("Routing not found!");
+        }
+    }
+}
